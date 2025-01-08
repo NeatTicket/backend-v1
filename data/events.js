f5ccd9afba00839788cdf4b43a1463ece54c61bd
0xmuka <hemasallam03@gmail.com>
2025-01-08 14:39:10 +0200

    data for simple testing before using databeses

---
+++ data/events.js
@@ -0,0 +1,14 @@
+let events = [
+    {
+        id: 1,
+        title: "ALX Egypt",
+        price: 1000
+    },
+    {
+        id: 2,
+        title: "ALX Event",
+        price: 500
+    },
+]
+
+module.exports = events
