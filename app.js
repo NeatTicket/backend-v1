7f58e016db456203745b9e5d43062176f9340101
0xmuka <hemasallam03@gmail.com>
2025-01-08 14:32:46 +0200

    (update) add new router for app.js
    - add endpoint /api/events
    - and update controller for this route

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const app = express()
 
 app.use(express.json())
 
-app.listen(3001, () => {
-    console.log("listen on port 3001")
+const eventRouter = require('./routes/eventsRoute');
+
+app.use('/api/events', eventRouter)
+
+app.listen(4000, () => {
+    console.log("listen on port 4000")
     
 })
